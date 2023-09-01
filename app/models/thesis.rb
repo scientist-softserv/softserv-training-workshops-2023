11df4a34676c20c834a6cadc3e66b79baf129ac3
Shana Moore <shana@scientist.com>
2023-09-01 12:20:27 -0700

    🤔 EXERCISE 4: DEFINING METADATA IN THE MODEL
    PART 1 - Add three metadata properties as defined in the
    comments/instructions.

--- app/models/thesis.rb
+++ app/models/thesis.rb
@@ -12,3 +12,26 @@ class Thesis < ActiveFedora::Base
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
 end
+
+# The GenericWork class is generated with some default metadata, but we want to
+# update it with our own metadata. 
+# 
+# BASIC AND CORE METADATA
+# Basic metadata are the default properties for all works.
+# ref: 
+# https://github.com/samvera/hyrax/blob/1-0-stable/app/models/concerns/hyrax/basic_metadata.rb
+# Core metadata properties are required and should never be removed! 
+# ref: 
+# https://github.com/samvera/hyrax/blob/1-0-stable/app/models/concerns/hyrax/required_metadata.rb
+# 
+# EXERCISE: EXTEND THE MODEL
+# Hint: Look at the basic metadata to see how the current properties are defined.
+# 1. Add a new single-value property called 'contact_email'
+# predicate: '::RDF::Vocab::VCARD.hasEmail'
+# index as ':stored_searchable'
+# 2. Add a new multi-value property called 'contact_phone'
+# predicate: '::RDF::Vocab::VCARD.hasTelephone'
+# index as ':stored_searchable, :facetable'
+# 3. Add a new property with a controlled vocabulary called 'department'
+# predicate: '::RDF::URI.new("http://lib.my.edu/departments")'
+# index as ':stored_searchable, :facetable'
