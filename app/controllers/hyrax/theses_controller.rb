6fe6d309555cdd81db22468ebace8d0be6fa917d
Shana Moore <shana@scientist.com>
2023-09-01 12:08:53 -0700

    💁🏿‍♀️ UNDERSTANDING THE CONTROLLER
    This commit adds comments to explain the controller and its options.

--- app/controllers/hyrax/theses_controller.rb
+++ app/controllers/hyrax/theses_controller.rb
@@ -12,3 +12,31 @@ module Hyrax
     self.show_presenter = Hyrax::ThesisPresenter
   end
 end
+
+# MODEL CLASS
+# The model class, which is part of the standard Rails 
+# Model-View-Controller, has its name determined by Rails convention.
+# self.curation_concern_type == the model name
+# model_name = singular controller_name minus 'Controller' (e.g. ::Thesis)
+# notice the controller name is plural by Rails convention.
+# controller_name = plural model_name plus 'Controller' (e.g. Theses)
+#
+# FORM CLASS
+# The form class is used to control how metadata appears on the new/edit work
+# form. A form class is created for each work type when the work type is
+# generated. The controller knows about this class through the
+# work_form_service.rb form_class method.
+# ref:
+# https://github.com/samvera/hyrax/blob/main/app/services/hyrax/work_form_service.rb
+# Default: form_class = model_name.name + Form (e.g. ThesisWorkForm)
+# Modifying: Although uncommon, you can change the class that is used as the
+# form class by setting it in the controller 
+# ie: self.form_class = MyCustomForm
+#
+# PRESENTER CLASS
+# The presenter class is used to control how metadata appears on the work show
+# page. This is automatically generated and it inherits from
+# Hyrax::WorkShowPresenter
+# Modifying: You can change the class that is used as the presenter class by
+# setting it in the controller.
+# ie: self.show_presenter = MyCustomPresenter
