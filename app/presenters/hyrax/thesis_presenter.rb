ea558b75d768ee7ac31c4bbb9b129518698e4d3f
Shana Moore <shana@scientist.com>
2023-09-01 13:53:12 -0700

    🎁 EXERCISE 9: DISPLAYED PROPERTIES
    This commit contains the code required to make the new properties appear
    on the Thesis work show page.

--- app/presenters/hyrax/thesis_presenter.rb
+++ app/presenters/hyrax/thesis_presenter.rb
@@ -2,6 +2,7 @@
 #  `rails generate hyrax:work Thesis`
 module Hyrax
   class ThesisPresenter < Hyrax::WorkShowPresenter
+    delegate :contact_email, :contact_phone, :department, to: :solr_document
   end
 end
 
